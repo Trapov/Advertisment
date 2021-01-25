a045a5cb183b89421d8ce8bcf220bf449acd8845
Дмитрий Афонин <d.afonin@solarl.ru>
2021-01-25 17:34:08 +0300

    Added domain.

---
+++ src/Advertisement.Domain/Ad.cs
@@ -0,0 +1,19 @@
+﻿using Advertisement.Domain.Shared;
+
+namespace Advertisement.Domain
+{
+    public sealed class Ad : MutableEntity<int>
+    {
+        public enum Statuses
+        {
+            Created,
+            Payed,
+            Closed
+        }
+        
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        
+        public Statuses Status { get; set; }
+    }
+}
---
+++ src/Advertisement.Domain/Shared/Exceptions/ConflictException.cs
@@ -0,0 +1,9 @@
+﻿namespace Advertisement.Domain.Shared.Exceptions
+{
+    public abstract class ConflictException : DomainException
+    {
+        protected ConflictException(string message) : base(message)
+        {
+        }
+    }
+}
